40532df0b96189739bb126788f3cfd122d297f1d
Xiyu-240 <2402444399@qq.com>
2024-02-29 15:20:46 +0800

    Update checkClick.cs

--- GC01/Assets/Scripts/checkClick.cs
+++ GC01/Assets/Scripts/checkClick.cs
@@ -11,14 +11,37 @@ public class checkClick : MonoBehaviour
     public float timerDuration = 10f; // 有效时间
     public float elapsedTime = 0.0f; // 已过时间
 
+    private bool inCD = false;// 判断是否冷却
+
+    public float CD = 5f;// 冷却
+    public float elapsedTime2 = 0.0f;// 已过
+
     // Update is called once per frame
     void Update()
     {
+        if (inCD)// 冷却中
+        {
+            elapsedTime2 += Time.fixedDeltaTime;
+
+            if (elapsedTime2 > CD)
+            {
+                inCD = false;
+                elapsedTime = 0.0f;
+            }
+        }
+        else//冷却完毕
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                inCD = true;
+            }
+        }
+
         if (isChecking)
         {
             elapsedTime += Time.fixedDeltaTime;// 计时
 
-            if (Input.GetKeyDown(KeyCode.Space))// 判断成功则调用盾反函数并结束检测
+            if (Input.GetKeyDown(KeyCode.Space) && !inCD)// 判断成功则调用盾反函数并结束检测
             {
                 manager.counterAttack();
                 endCheck();

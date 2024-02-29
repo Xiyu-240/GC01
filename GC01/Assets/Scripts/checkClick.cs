aeef439a56366d519897601ade905a5013b6d19c
Xiyu-240 <2402444399@qq.com>
2024-02-29 13:32:54 +0800

    Animation beta01
    Probably a buggy beta

--- GC01/Assets/Scripts/checkClick.cs
+++ GC01/Assets/Scripts/checkClick.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class checkClick : MonoBehaviour
 {
+    public Animator enemyAnimator;
+    public Animator playerAnimator;
+
     private bool isChecking = false;// 判断是否在检测
 
     public float timerDuration = 1.0f; // 有效时间
-    private float elapsedTime = 0.0f; // 已过时间
+    public float elapsedTime = 0.0f; // 已过时间
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +23,14 @@ public class checkClick : MonoBehaviour
     {
         if (isChecking)
         {
-            elapsedTime += Time.deltaTime;// 计时
+            elapsedTime += Time.fixedDeltaTime;// 计时
 
-            if (Input.GetButtonDown("Space"))// 判断成功则调用盾反函数并结束检测
+            if (Input.GetKeyDown(KeyCode.Space))// 判断成功则调用盾反函数并结束检测
             {
                 counterAttack();
                 endCheck();
             }
-            else if (elapsedTime>timerDuration)// 若计时大于有效时间，调用受击函数并结束检测
+            else if (elapsedTime > timerDuration)// 若计时大于有效时间，调用受击函数并结束检测
             {
                 underAttack();
                 endCheck();
@@ -48,10 +51,23 @@ public class checkClick : MonoBehaviour
 
     public void counterAttack()// 盾反函数
     {
-
+        Debug.Log("1");
+        enemyAnimator.SetBool("achieve", true);
+        playerAnimator.SetBool("counter", true);
     }
     public void underAttack()// 受击函数
     {
+        playerAnimator.SetBool("injured", true);
+    }
 
+    public void endAnimation()// 重置动画状态
+    {
+        enemyAnimator.SetBool("achieve", false);
+        playerAnimator.SetBool("counter", false);
+    }
+
+    public void endInjured()// 这实属无奈，一会可以添到player脚本或许？
+    {
+        playerAnimator.SetBool("injured", false);
     }
 }
